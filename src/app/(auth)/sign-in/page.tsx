58a5fd7d6bca0ce391a67588af9359124314f4e9
Rawan Almujahed <rawanalmujahed.2002@gmail.com>
2026-01-29 20:35:43 +0300

    Remove all project files

--- src/app/(auth)/sign-in/page.tsx
+++
@@ -1,6 +0,0 @@
-import AuthForm from "@/components/AuthForm";
-import {signIn} from "@/lib/auth/actions";
-
-export default function Page() {
-  return <AuthForm mode="sign-in" onSubmit={signIn} />;
-}
--- src/app/(auth)/sign-up/page.tsx
+++
@@ -1,6 +0,0 @@
-import AuthForm from "@/components/AuthForm";
-import {signUp} from "@/lib/auth/actions";
-
-export default function Page() {
-  return <AuthForm mode="sign-up" onSubmit={signUp} />;
-}
